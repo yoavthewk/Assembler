d3488827e9d4669c495a7a7cbc99e8557d669dbe
yoavthewk <yoavarviv1@gmail.com>
2022-01-27 19:31:10 +0200

    valgrind

--- Hardware/hardware.h
+++ Hardware/hardware.h
@@ -15,6 +15,30 @@ typedef struct PSW{
     int Z : 1;
 }PSW;
 
+struct{
+	char *name;
+	int op_code : 16;
+	int funct : 4;
+	}action_table[] = {
+		{"mov", 0, NULL},
+		{"cmp", 1, NULL},
+		{"add", 2, 10},
+		{"sub", 2, 11}, 
+		{"lea", 8, NULL}, 
+		{"clr", 16, 10}, 
+		{"not", 16, 11}, 
+		{"inc", 16, 12}, 
+		{"dec", 16, 13},
+		{"jmp", 256, 10},
+		{"bne", 256, 11},
+		{"jsr", 256, 12},
+		{"red", 2048, NULL},
+		{"prn", 4096, NULL},
+		{"rts", 8192, NULL},
+		{"stop", 16385, NULL},
+		{"not_valid", NULL}
+	};
+
 word memory[MEMORY_SIZE] = {0};
 hregister registers[NUM_OF_REGISTERS] = {0};
 hregister PC = {100};
