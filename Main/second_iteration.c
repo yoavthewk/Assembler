ff6b73c5fa4c37f7229c02e26b5d0b6f1fa0dbb5
liamdrop205 <liamdrop205@gmail.com>
2022-03-27 17:24:38 +0300

    Now deleting files properly

--- Main/second_iteration.c
+++ Main/second_iteration.c
@@ -11,11 +11,13 @@ void second_iteration(char *file_name, FILE *fp, int ICF, int DCF, symbol_list *
 
     while ((line = get_next_line(fp)) != NULL)
     {
-        if(flag_register->ERR) flag_register->ENC = 1;
+        if (flag_register->ERR)
+            flag_register->ENC = 1;
         second_line_process(extfp, line, ICF, line_number++, head, command_head, flag_register);
         free(line);
     }
-    if(!flag_register->ENC){
+    if (!flag_register->ENC)
+    {
         print_command_list(command_head);
         format_object_file(obfp, ICF, DCF, command_head);
         write_entry_to_file(entfp, head);
@@ -23,16 +25,13 @@ void second_iteration(char *file_name, FILE *fp, int ICF, int DCF, symbol_list *
     fclose(obfp);
     fclose(extfp);
     fclose(entfp);
-    delete_extern_file(file_name);
-    delete_entry_file(file_name);
-    delete_am_file(file_name);
 }
 
 void second_line_process(FILE *fp, char *line, int ICF, int line_number, symbol_list *head, command_list *command_head, PSW *flag_register)
 {
     char line_backup[MAX_LEN] = {0};
     char *tok;
-    char* label = NULL;
+    char *label = NULL;
     static int IC = 100;
     int offset = 0;
     flag_register->ERR = flag_register->SYM = 0;
@@ -49,7 +48,8 @@ void second_line_process(FILE *fp, char *line, int ICF, int line_number, symbol_
 
     contains_label(line_backup, head, line_number, flag_register);
     strcpy(line_backup, line);
-    if (flag_register->SYM) {
+    if (flag_register->SYM)
+    {
         tok = strtok(line_backup, " ");
         offset += strlen(tok) + 1;
     }
@@ -80,9 +80,10 @@ void second_line_process(FILE *fp, char *line, int ICF, int line_number, symbol_
         return;
     }
     strcpy(line_backup, line);
-    /* if we got this far, it is a command */    
+    /* if we got this far, it is a command */
     /* check if we need to complete words */
-    if(!need_completion(command_head, IC)){
+    if (!need_completion(command_head, IC))
+    {
         IC = get_next_IC(IC, command_head);
         /* if not, we return */
         free(line);
@@ -99,29 +100,33 @@ void second_line_process(FILE *fp, char *line, int ICF, int line_number, symbol_
     /* get the first operand */
     tok = strtok(line, ",");
     /* if it starts with # it's an immediate, and we need to encode the second operand. */
-    label = (char*)calloc(MAX_LEN, sizeof(char));
-    if(tok[0] == '#')
+    label = (char *)calloc(MAX_LEN, sizeof(char));
+    if (tok[0] == '#')
         strcpy(label, strtok(NULL, ","));
-    
-    /* check if it's a register direct */
-    else if(tok[0] == 'r'){
-        int j = 0, i = 1, register_number = 0;
-	    char number[MAX_LEN] = {0};
 
-	    while ((isdigit(tok[i]) || tok[i] == '-' || tok[i] == '+') && tok[i] != ',')
-		    number[j++] = tok[i++];
+    /* check if it's a register direct */
+    else if (tok[0] == 'r')
+    {
+        int j = 0, i = 1, register_number = 0;
+        char number[MAX_LEN] = {0};
+
+        while ((isdigit(tok[i]) || tok[i] == '-' || tok[i] == '+') && tok[i] != ',')
+            number[j++] = tok[i++];
 
         register_number = atoi(number);
-        if(register_number >= 0 && register_number <= 15){
+        if (register_number >= 0 && register_number <= 15)
+        {
             strcpy(label, strtok(NULL, ","));
         }
     }
-    else{
+    else
+    {
         strcpy(label, tok);
     }
 
     fill_command_list(head, &command_head, flag_register, fp, label, IC);
-    if(flag_register->ERR){
+    if (flag_register->ERR)
+    {
         throw_error("Label does not exist!", line_number);
     }
     IC = get_next_IC(IC, command_head);
@@ -129,11 +134,12 @@ void second_line_process(FILE *fp, char *line, int ICF, int line_number, symbol_
     free(line);
 }
 
-int get_next_IC(int IC, command_list* head)
+int get_next_IC(int IC, command_list *head)
 {
     while (head)
     {
-        if (IC < head->IC) return head->IC;
+        if (IC < head->IC)
+            return head->IC;
         head = head->next;
     }
     return 0;
@@ -143,20 +149,21 @@ bool need_completion(command_list *head, int IC)
 {
     do
     {
-        if (head->IC == IC && head->L > 3){
+        if (head->IC == IC && head->L > 3)
+        {
             return true;
         }
     } while ((head = head->next));
     return false;
 }
 
-void fill_command_list(symbol_list *head, command_list **command_head, PSW* flag_register, FILE* fp, char* label, int IC)
+void fill_command_list(symbol_list *head, command_list **command_head, PSW *flag_register, FILE *fp, char *label, int IC)
 {
     int i;
-    char* is_index = strchr(label, '[');
+    char *is_index = strchr(label, '[');
     bool found = false;
-    command_list * tmp = * command_head;
-    char* substr = NULL;
+    command_list *tmp = *command_head;
+    char *substr = NULL;
 
     if (is_index)
     {
@@ -167,31 +174,35 @@ void fill_command_list(symbol_list *head, command_list **command_head, PSW* flag
 
     do
     {
-        if (tmp->IC == IC){
+        if (tmp->IC == IC)
+        {
             break;
         }
     } while ((tmp = tmp->next));
 
     do
     {
-        if (!strcmp(label, head->s.name)){
+        if (!strcmp(label, head->s.name))
+        {
             found = true;
             break;
         }
     } while ((head = head->next));
 
-    if(!found){
+    if (!found)
+    {
         flag_register->ERR = 1;
         return;
     }
 
-    for(i = 0; tmp->arr[i][0] != '?'; i++)
+    for (i = 0; tmp->arr[i][0] != '?'; i++)
         ;
 
-    if(head->s.attributes[EXTERN]){
+    if (head->s.attributes[EXTERN])
+    {
         write_extern_to_file(fp, head->s.name, tmp->IC + i, tmp->IC + i + 1);
     }
-    
+
     free(tmp->arr[i]);
     tmp->arr[i++] = encode_label_value(head->s.value - head->s.value % 16, head->s.attributes[EXTERN]);
     free(tmp->arr[i]);
@@ -216,7 +227,8 @@ void handle_entry(char *line, symbol_list *head, PSW *flag_register, int line_nu
     /* skip the .entry */
     strtok(line, " ");
 
-    if(flag_register->SYM){
+    if (flag_register->SYM)
+    {
         strtok(NULL, " ");
     }
 
@@ -229,25 +241,26 @@ void handle_entry(char *line, symbol_list *head, PSW *flag_register, int line_nu
         flag_register->ERR = 1;
         return;
     }
-    else if(flag_register->SYM)
+    else if (flag_register->SYM)
     {
         printf("Warning in line %d: Label and entry are declared in the same line.", line_number);
     }
-    
+
     update_entry(head, label_name);
 }
 
-void write_entry_to_file(FILE* fp, symbol_list *head)
+void write_entry_to_file(FILE *fp, symbol_list *head)
 {
     do
     {
-        if (head->s.attributes[ENTRY]){
+        if (head->s.attributes[ENTRY])
+        {
             fprintf(fp, "%s,%04d,%04d\n", head->s.name, head->s.base_address, head->s.offset);
         }
     } while ((head = head->next));
 }
 
-void write_extern_to_file(FILE* fp, char* name, int address, int second_address)
+void write_extern_to_file(FILE *fp, char *name, int address, int second_address)
 {
     fprintf(fp, "%s BASE %04d\n", name, address);
     fprintf(fp, "%s OFFSET %04d\n\n", name, second_address);
@@ -266,7 +279,7 @@ void format_object_file(FILE *fp, int IC, int DC, command_list *head)
     char str[WORD_SIZE] = {0}; /* creating a temporary string to hold the integers */
     int i = 0, cond = get_command_size(head), j = 0;
 
-    sprintf(str, "%d", (IC - 100));         /* converting IC to char* */
+    sprintf(str, "%d", (IC - 100)); /* converting IC to char* */
     append_to_object_file(fp, str); /* appending it to the file */
 
     append_to_object_file(fp, " "); /* adding space */
